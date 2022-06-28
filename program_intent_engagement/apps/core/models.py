13624ecd653f44b76ff538b420079f392b6e55a7
Varsha Menon <vmenon@h6ydh4n6wl.2tor.net>
2022-06-28 09:48:33 -0400

    added todos

--- program_intent_engagement/apps/core/models.py
+++ program_intent_engagement/apps/core/models.py
@@ -3,6 +3,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
+# TODO: ask alie how to import (using requirements)
 
 
 class User(AbstractUser):
@@ -36,3 +38,43 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.get_full_name())
+
+
+class ProgramIntent(TimeStampedModel):
+    """
+    Information about the Program Intent.
+
+    .. no_pii:
+    """
+
+    # TODO: how should this be displayed?
+    class IntentCertainty(models.TextChoices):
+        CERTAIN_YES = 'Y', _('Certain Yes')
+        CERTAIN_NO = 'N', _('Certain No')
+        MAYBE = 'M', _('Certain Maybe')
+
+    # TODO: read about db index and on delete
+    user = models.ForeignKey(User, db_index=True, on_delete=models.CASCADE)
+
+    program = models.UUIDField()
+
+    # The specific reason for how intent was measured.
+    reason = models.CharField(max_length=255, db_index=True)
+
+    # TODO: would a default be useful?
+    # The certainty of the program intent for the user.
+    certainty = models.CharField(max_length=1, choices=IntentCertainty.choices)
+
+    # When the event that made this intent occurred.
+    effective_time = models.DateTimeField(null=True)
+
+    # TODO: is meta naming correct?
+    class Meta:
+        """ Meta class for this Django model """
+        db_table = 'pie_program_intent'
+        verbose_name = 'program_intent'
+        # TODO: is this the right unique together?
+        # TODO: why is there an extra comma?
+        unique_together = (('user', 'program'),)
+
+
