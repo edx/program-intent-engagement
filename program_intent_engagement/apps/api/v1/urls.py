85d1c87cdc948793d54424d97570aac110e11a24
m-pitera <mpitera483@gmail.com>
2022-07-05 19:06:23 +0000

    feat: added get endpoint for best intents
    feat: added serializer for ProgramIntent model
    feat: added url path
    -fix: url now uses re_path
    
    test: adding tests for get endpoint
    -fix: added jwt_auth to settings
    test: add tests for get endpoint
    test: added test_utils
    
    chore: adding requirements
    -added ddt and factory-boy for testing
    
    chore: added files to .gitignore
    -added .vscode which is VSCode's config file
    -added reports/ which seems like it's autogenerated test output

--- program_intent_engagement/apps/api/v1/urls.py
+++ program_intent_engagement/apps/api/v1/urls.py
@@ -1,12 +1,19 @@
 """ API v1 URLs. """
 from django.urls import re_path
 
-from program_intent_engagement.apps.api.v1.views import ProgramIntentsView
+from program_intent_engagement.apps.api.v1.views import MostRecentAndCertainIntentsView, ProgramIntentsView
 
-app_name = 'v1'
+app_name = "v1"
 
 urlpatterns = [
-    re_path(r'^program_intents/?$',
-            ProgramIntentsView.as_view(),
-            name='program_intent-insert'),
+    re_path(
+        r"^program_intents/?$",
+        ProgramIntentsView.as_view(),
+        name="program_intent-insert",
+    ),
+    re_path(
+        r"^intents/most-recent-and-certain?$",
+        MostRecentAndCertainIntentsView.as_view(),
+        name="intents-list",
+    ),
 ]
